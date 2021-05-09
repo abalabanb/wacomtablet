391aa69b7f59831a60e8563bec2c15cf98248eec
Alexandre Balaban <30463679+abalabanb@users.noreply.github.com>
2021-05-10 01:01:39 +0200

    Fix version file

--- Sources/WacomTablet.usbfd_rev.h
+++ Sources/WacomTablet.usbfd_rev.h
@@ -1,16 +1,16 @@
-#define VERSION         1
-#define REVISION        1
-#define SUBREVISION     110
+#define VERSION			1
+#define REVISION		1
+#define SUBREVISION		111
 
-#define DATE            "09.05.2021"
-#define VERS            "WacomTablet.usbfd 1.1"
-#define VSTRING         "WacomTablet.usbfd 1.1 (09.05.2021)\r\n"
-#define VERSTAG         "\0$VER: WacomTablet.usbfd 1.1 (09.05.2021)"
+#define DATE			"10.05.2021"
+#define VERS			"WacomTablet.usbfd 1.1"
+#define VSTRING			"WacomTablet.usbfd 1.1 (10.05.2021)\r\n"
+#define VERSTAG			"\0$VER: WacomTablet.usbfd 1.1 (10.05.2021)"
 
-#define VERSION_STR     "1"
-#define REVISION_STR        "1"
-#define SUBREVISION_STR     "110"
+#define VERSION_STR		"1"
+#define REVISION_STR		"1"
+#define SUBREVISION_STR		"111"
 
-#define NEW_VERS        "WacomTablet.usbfd 1.1.110"
-#define NEW_VSTRING     "WacomTablet.usbfd 1.1.110 (09.05.2021)\r\n"
-#define NEW_VERSTAG     "\0$VER: WacomTablet.usbfd 1.1.110 (09.05.2021)"
+#define NEW_VERS		"WacomTablet.usbfd 1.1.111"
+#define NEW_VSTRING		"WacomTablet.usbfd 1.1.111 (10.05.2021)\r\n"
+#define NEW_VERSTAG		"\0$VER: WacomTablet.usbfd 1.1.111 (10.05.2021)"
