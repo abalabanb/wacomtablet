17ab3f59f6d34c2a5e71be10876b06627b1ebd32
Alexandre Balaban <30463679+abalabanb@users.noreply.github.com>
2024-05-01 18:50:18 +0200

    Forgotten revision file

--- Sources/WacomTablet.usbfd_rev.h
+++ Sources/WacomTablet.usbfd_rev.h
@@ -1,16 +1,16 @@
 #define VERSION			1
 #define REVISION		1
-#define SUBREVISION		125
+#define SUBREVISION		154
 
-#define DATE			"19.02.2022"
+#define DATE			"27.04.2024"
 #define VERS			"WacomTablet.usbfd 1.1"
-#define VSTRING			"WacomTablet.usbfd 1.1 (19.02.2022)\r\n"
-#define VERSTAG			"\0$VER: WacomTablet.usbfd 1.1 (19.02.2022)"
+#define VSTRING			"WacomTablet.usbfd 1.1 (27.04.2024)\r\n"
+#define VERSTAG			"\0$VER: WacomTablet.usbfd 1.1 (27.04.2024)"
 
 #define VERSION_STR		"1"
 #define REVISION_STR		"1"
-#define SUBREVISION_STR		"125"
+#define SUBREVISION_STR		"154"
 
-#define NEW_VERS		"WacomTablet.usbfd 1.1.125"
-#define NEW_VSTRING		"WacomTablet.usbfd 1.1.125 (19.02.2022)\r\n"
-#define NEW_VERSTAG		"\0$VER: WacomTablet.usbfd 1.1.125 (19.02.2022)"
+#define NEW_VERS		"WacomTablet.usbfd 1.1.154"
+#define NEW_VSTRING		"WacomTablet.usbfd 1.1.154 (27.04.2024)\r\n"
+#define NEW_VERSTAG		"\0$VER: WacomTablet.usbfd 1.1.154 (27.04.2024)"
